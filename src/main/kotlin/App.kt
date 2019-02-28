599353e8fa1bce3ca50b0aa896065a6f888e8cd4
san6685 <aroteswati@crest.in>
2019-02-28 13:13:55 +0530

    refactored code

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,24 +1,7 @@
-import org.jooq.Field
-import org.jooq.Record
-import org.jooq.Table
-import org.jooq.impl.DSL.field
-import org.jooq.impl.DSL.table
-
-
 class App{
+    fun main(args: Array<String>) {
 
-//    companion object{
-//        val JOURNAL: Table<Record> = table("journal")
-//        val JOU_ID: Field<Int> = field("jou_id", Int::class.java)
-//        val JOU_NAME: Field<String> = field("jou_name", String::class.java)
-//
-//        val ARTICLETYPES: Table<Record> = table("articletypes")
-//        val MST_ID: Field<Int> = field("mst_id", Int::class.java)
-//        val MST_JOU_ID: Field<Int> = field("mst_jou_id", Int::class.java)
-//        val MST_NAME: Field<String> = field("mst_name", String::class.java)
-//    }
+    }
 }
-fun main(args: Array<String>) {
 
-}
 
